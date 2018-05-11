da6a87af77156f40cf14a8ac52a06d6ed73004f8
Shekhar Sarkar <shekharsarkar138@gmail.com>
2018-05-11 13:59:57 -0400

    Initial Workspace upload

---
+++ src/main/java/com/demo/model/UserProfileType.java
@@ -0,0 +1,19 @@
+package com.demo.model;
+
+
+public enum UserProfileType {
+    USER("USER"),
+    DBA("DBA"),
+    ADMIN("ADMIN");
+     
+    String userProfileType;
+     
+    private UserProfileType(String userProfileType){
+        this.userProfileType = userProfileType;
+    }
+     
+    public String getUserProfileType(){
+        return userProfileType;
+    }
+     
+}
